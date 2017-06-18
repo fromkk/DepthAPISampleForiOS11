f63592a09ddbf8e68ac5bd1224a837dc9c881413
Kazuya Ueoka <info@fromkk.info>
2017-06-18 15:26:35 +0900

    add mode

--- DepthSample/DepthViewController.swift
+++ DepthSample/DepthViewController.swift
@@ -10,69 +10,37 @@ import UIKit
 import Photos
 
 class DepthViewController: UIViewController {
+    
+    enum Mode {
+        case disparity
+        case chromakey
+        
+        var toString: String {
+            switch self {
+            case .disparity:
+                return "Disparity"
+            case .chromakey:
+                return "Chroma key"
+            }
+        }
+    }
+    var mode: Mode? {
+        didSet {
+            guard let mode: Mode = self.mode else { return }
+            
+            switch mode {
+            case .disparity:
+                self.loadDisparityImage()
+            case .chromakey:
+                self.loadDisparityWithChromakey()
+            }
+        }
+    }
+    
     var asset: PHAsset!
     
     private lazy var imageManager: PHImageManager = PHImageManager()
-    
-    private func loadBaseImage() {
-        let options: PHImageRequestOptions = PHImageRequestOptions()
-        options.isNetworkAccessAllowed = true
-        options.deliveryMode = .highQualityFormat
-        
-        self.imageManager.requestImage(for: self.asset, targetSize: CGSize(width: UIScreen.main.bounds.size.width * UIScreen.main.scale, height: UIScreen.main.bounds.size.height * UIScreen.main.scale), contentMode: .aspectFit, options: options) { (image, info) in
-            DispatchQueue.main.async {
-                self.baseImageView.image = image
-                self.loadDisparityImageView()
-            }
-        }
-    }
-    
-    private func loadDisparityImageView() {
-        self.asset.requestContentEditingInput(with: nil) { (input, info) in
-            guard let imageURL: URL = input?.fullSizeImageURL else {
-                return
-            }
-            
-            if let disparityImage: CIImage = CIImage(contentsOf: imageURL, options: [kCIImageAuxiliaryDisparity: true]) {
-                self.handleDisparity(with: disparityImage.applyingFilter("CIDisparityToDepth", withInputParameters: nil))
-            } else if let depthImage: CIImage = CIImage(contentsOf: imageURL, options: [kCIImageAuxiliaryDepth: true]) {
-                self.handleDepth(with: depthImage)
-            }
-        }
-    }
-    
-    private func handleDisparity(with disparityImage: CIImage) {
-        let disparityUIImage: UIImage = UIImage(ciImage: disparityImage)
-        
-        DispatchQueue.main.async {
-            guard let image: UIImage = self.baseImageView.image?.resizedImage(with: disparityUIImage.size),
-                let ciImage: CIImage = CIImage(image: image) else {
-                print(#function, "image cannot load")
-                return
-            }
-            guard let colorImage: UIImage = UIColor.red.toImage(with: disparityUIImage.size) else {
-                print(#function, "colorImage create failed")
-                return
-            }
-            
-            guard let colorCIImage: CIImage = CIImage(image: colorImage) else {
-                print(#function, "colorImage cannot load")
-                return
-            }
-            
-            let maskedImage: CIImage = ciImage.applyingFilter("CIBlendWithMask", withInputParameters: [
-                kCIInputBackgroundImageKey: colorCIImage,
-                kCIInputMaskImageKey: disparityImage.applyingFilter("CIColorClamp", withInputParameters: nil),
-                ])
-            self.baseImageView.image = UIImage(ciImage: maskedImage)
-        }
-    }
-    
-    private func handleDepth(with ciImage: CIImage) {
-        print(#function)
-        
-//        self.disparityImageView.image = UIImage(ciImage: ciImage)
-    }
+    private lazy var menuButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(self.handle(menuButton:)))
     
     lazy var baseImageView: UIImageView = { () -> UIImageView in
         let imageView: UIImageView = UIImageView(frame: .zero)
@@ -93,8 +61,9 @@ class DepthViewController: UIViewController {
         
         self.title = "Depth detail"
         self.navigationItem.largeTitleDisplayMode = .never
-        self.view.backgroundColor = .white
+        self.navigationItem.rightBarButtonItem = self.menuButton
         
+        self.view.backgroundColor = .white
         self.view.addSubview(self.baseImageView)
         self.view.addSubview(self.disparityImageView)
         
@@ -110,7 +79,82 @@ class DepthViewController: UIViewController {
             ])
         
         self.loadBaseImage()
-//        self.loadDisparityImageView()
     }
     
+    @objc private func handle(menuButton: UIBarButtonItem) {
+        let modesViewController: ModeViewController = ModeViewController()
+        modesViewController.modeSelected { [weak self] (mode) in
+            self?.mode = mode
+        }
+        
+        let navigationController: UINavigationController = UINavigationController(rootViewController: modesViewController)
+        self.present(navigationController, animated: true, completion: nil)
+    }
+}
+
+extension DepthViewController {
+    fileprivate func loadBaseImage() {
+        let options: PHImageRequestOptions = PHImageRequestOptions()
+        options.isNetworkAccessAllowed = true
+        options.deliveryMode = .highQualityFormat
+        
+        self.imageManager.requestImage(for: self.asset, targetSize: CGSize(width: UIScreen.main.bounds.size.width * UIScreen.main.scale, height: UIScreen.main.bounds.size.height * UIScreen.main.scale), contentMode: .aspectFit, options: options) { (image, info) in
+            DispatchQueue.main.async {
+                self.baseImageView.image = image
+            }
+        }
+    }
+    
+    fileprivate func loadDisparityImage() {
+        self.asset.requestContentEditingInput(with: nil) { (input, info) in
+            guard let imageURL: URL = input?.fullSizeImageURL else {
+                return
+            }
+            
+            if let disparityImage: CIImage = CIImage(contentsOf: imageURL, options: [kCIImageAuxiliaryDisparity: true]) {
+                DispatchQueue.main.async {
+                    self.disparityImageView.image = UIImage(ciImage: disparityImage)
+                }
+            }
+        }
+    }
+    
+    fileprivate func loadDisparityWithChromakey() {
+        self.asset.requestContentEditingInput(with: nil) { (input, info) in
+            guard let imageURL: URL = input?.fullSizeImageURL else {
+                return
+            }
+            
+            if let disparityImage: CIImage = CIImage(contentsOf: imageURL, options: [kCIImageAuxiliaryDisparity: true]) {
+                self.handleDisparity(with: disparityImage)
+            }
+        }
+    }
+    
+    private func handleDisparity(with disparityImage: CIImage) {
+        let disparityUIImage: UIImage = UIImage(ciImage: disparityImage)
+        
+        DispatchQueue.main.async {
+            guard let image: UIImage = self.baseImageView.image?.resizedImage(with: disparityUIImage.size),
+                let ciImage: CIImage = CIImage(image: image) else {
+                    print(#function, "image cannot load")
+                    return
+            }
+            guard let colorImage: UIImage = UIColor.red.toImage(with: disparityUIImage.size) else {
+                print(#function, "colorImage create failed")
+                return
+            }
+            
+            guard let colorCIImage: CIImage = CIImage(image: colorImage) else {
+                print(#function, "colorImage cannot load")
+                return
+            }
+            
+            let maskedImage: CIImage = ciImage.applyingFilter("CIBlendWithMask", withInputParameters: [
+                kCIInputBackgroundImageKey: colorCIImage,
+                kCIInputMaskImageKey: disparityImage.applyingFilter("CIColorClamp", withInputParameters: nil),
+                ])
+            self.disparityImageView.image = UIImage(ciImage: maskedImage)
+        }
+    }
 }
---
+++ DepthSample/ModeViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ModeViewController.swift
+//  DepthSample
+//
+//  Created by Kazuya Ueoka on 2017/06/18.
+//  Copyright © 2017 fromKK. All rights reserved.
+//
+
+import UIKit
+
+class ModeViewController: UIViewController {
+    typealias ModeSelected = (DepthViewController.Mode) -> ()
+    private var _modeSelected: ModeSelected?
+    func modeSelected(_ modeSelected: @escaping ModeSelected) {
+        self._modeSelected = modeSelected
+    }
+    
+    let modes: [DepthViewController.Mode] = [.disparity, .chromakey]
+    
+    lazy var tableView: UITableView = { () -> UITableView in
+        let tableView: UITableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(Cell.self, forCellReuseIdentifier: Cell.reuseIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
+    lazy var closeButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.handle(closeButton:)))
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.title = "Mode"
+        self.navigationItem.leftBarButtonItem = self.closeButton
+        
+        self.view.backgroundColor = .white
+        self.view.addSubview(self.tableView)
+        self.tableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1.0, constant: 0.0).isActive = true
+        self.tableView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1.0, constant: 0.0).isActive = true
+        self.tableView.centerXAnchor.anchorWithOffset(to: self.view.centerXAnchor).constraint(equalToConstant: 0.0).isActive = true
+        self.tableView.centerYAnchor.anchorWithOffset(to: self.view.centerYAnchor).constraint(equalToConstant: 0.0).isActive = true
+    }
+    
+    @objc private func handle(closeButton: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ModeViewController {
+    class Cell: UITableViewCell {
+        static let reuseIdentifier: String = "ModeCell"
+        
+        override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            
+            self.setup()
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            
+            self.setup()
+        }
+        
+        private var isSetuped: Bool = false
+        private func setup() {
+            guard !self.isSetuped else { return }
+            defer { self.isSetuped = true }
+            
+            self.contentView.addSubview(self.titleLabel)
+            NSLayoutConstraint.activate([
+                NSLayoutConstraint(item: self.titleLabel, attribute: .leading, relatedBy: .equal, toItem: self.contentView, attribute: .leading, multiplier: 1.0, constant: 16.0),
+                NSLayoutConstraint(item: self.titleLabel, attribute: .trailing, relatedBy: .equal, toItem: self.contentView, attribute: .trailing, multiplier: 1.0, constant: 16.0),
+                NSLayoutConstraint(item: self.titleLabel, attribute: .top, relatedBy: .equal, toItem: self.contentView, attribute: .top, multiplier: 1.0, constant: 8.0),
+                NSLayoutConstraint(item: self.titleLabel, attribute: .bottom, relatedBy: .lessThanOrEqual, toItem: self.contentView, attribute: .bottom, multiplier: 1.0, constant: -16.0),
+                ])
+        }
+        
+        lazy var titleLabel: UILabel = { () -> UILabel in
+            let label: UILabel = UILabel(frame: .zero)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+    }
+}
+
+extension ModeViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.modes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: Cell.reuseIdentifier, for: indexPath)
+        if let cell: Cell = cell as? Cell {
+            cell.titleLabel.text = self.modes[indexPath.row].toString
+        }
+        return cell
+    }
+}
+
+extension ModeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self._modeSelected?(self.modes[indexPath.row])
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+}
